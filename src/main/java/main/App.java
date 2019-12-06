2c22817707e7a74c78c8779fc53b89f010b6d9a8
Rebekah Radcliffe <rebekah.zoe.radcliffe@gmail.com>
2019-12-06 07:52:26 +0000

    player coordinates created

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -5,6 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println(TreasureCoordinates.getxCoordinate());
 		System.out.println(TreasureCoordinates.getyCoordinate());
+		System.out.println(PlayerCoordinates.getxCoordinate());
+		System.out.println(PlayerCoordinates.getyCoordinate());
 	}
 
 }
---
+++ src/main/java/main/PlayerCoordinates.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Random;
+
+public class PlayerCoordinates {
+	static Random rand = new Random();
+	private static int xCoordinate = rand.nextInt(11);
+	private static int yCoordinate = rand.nextInt(11);
+	public static int getxCoordinate() {
+		return xCoordinate;
+	}
+	public static void setxCoordinate(int xCoordinate) {
+		PlayerCoordinates.xCoordinate = xCoordinate;
+	}
+	public static int getyCoordinate() {
+		return yCoordinate;
+	}
+	public static void setyCoordinate(int yCoordinate) {
+		PlayerCoordinates.yCoordinate = yCoordinate;
+	}
+	
+	
+
+}
